b54b02bdf0b7ab2b8ed8862d529b85284aa0e992
caok <caok1231@gmail.com>
2014-09-22 23:30:53 +0800

    update for db migrate

--- db/migrate/20140922064352_change_price_to_integer.rb
+++ db/migrate/20140922064352_change_price_to_integer.rb
@@ -1,5 +1,9 @@
 class ChangePriceToInteger < ActiveRecord::Migration
   def up
+    Entry.all.each do |e|
+      e.price = e.price.to_s.match(/\d{1,}/).to_s
+    end
+
     change_column :entries, :price, 'integer USING CAST(price AS integer)'
   end
 end
