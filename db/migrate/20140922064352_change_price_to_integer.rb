d29d75120225ccc1689027c13d1c108e1f466a7b
caok <caok1231@gmail.com>
2014-09-22 23:37:09 +0800

    fix the bug of migration for change price to integer

--- db/migrate/20140922064352_change_price_to_integer.rb
+++ db/migrate/20140922064352_change_price_to_integer.rb
@@ -2,6 +2,7 @@ class ChangePriceToInteger < ActiveRecord::Migration
   def up
     Entry.all.each do |e|
       e.price = e.price.to_s.match(/\d{1,}/).to_s
+      e.save!
     end
 
     change_column :entries, :price, 'integer USING CAST(price AS integer)'
