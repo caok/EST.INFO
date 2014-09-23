961e1b34b120434ea45e99b74e82988869c355a2
xiongbo <xiongbo027@gmail.com>
2014-09-23 11:18:38 +0800

    fixed history prices style

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,7 +14,10 @@ class EntriesController < ApplicationController
   private
   def history_prices(entries)
     history_hash = {}
-    history_prices = entries.where("created_at >= ?", 30.day.ago).group_by_day(:created_at, time_zone: "Beijing").order("day desc").average(:price)
-    history_prices
+    history_max = entries.where("created_at >= ?", 30.day.ago).group_by_day(:created_at, time_zone: "Beijing").order("day").maximum(:price)
+    history_min = entries.where("created_at >= ?", 30.day.ago).group_by_day(:created_at, time_zone: "Beijing").order("day").minimum(:price)
+    history_hash.store(:max, history_max)
+    history_hash.store(:min, history_min)
+    history_hash
   end
 end
